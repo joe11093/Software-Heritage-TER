c3e8014e22c6a9c1152b37f27a07e65c175172c8
joe11093 <joe11093@users.noreply.github.com>
2019-05-03 16:26:16 +0200

    creating the index in the contructor + fixing the response_simplified function

--- src/launchpad/launchpad_git/LaunchpadGitLister.py
+++ src/launchpad/launchpad_git/LaunchpadGitLister.py
@@ -1,15 +1,39 @@
+import os
+from time import time
+from datetime import datetime
 
 from swh.lister.core.lister_base import SWHListerBase
 from swh.lister.launchpad_git.models import LaunchpadGitModel
+from swh.lister.launchpad_git import LaunchpadProxy, DEFAULT_JSON_PATH
 
 class LaunchpadGitLister(SWHListerBase):
 	MODEL = LaunchpadGitModel
 	LISTER_NAME = 'launchpad_git'
 	#PROXY = None
 
-	def __init__(self, api_proxy, override_config=None):
+	def __init__(self, override_config=None):
+		#calling the parent constructor
+		SWHListerBase.__init__(self, override_config)
 		#instance of the proxy class
-		self.api_proxy = api_proxy
+		self.api_proxy = new LaunchpadProxy()
+		#logging in anonymously to the launchpad API
+		api_proxy.login("SWH access", "production", "devel")
+		#calling the proxy's indexing method
+		#will call the right index building based on whether the index file exists or not
+		if(os.path.isfile(DEFAULT_JSON_PATH)):
+			print("\nLazy index building at {}".format(datetime.now()))
+		    start_time = time()
+		    proxy.lazy_index_build()
+		    print("Built at {} (took {} seconds)".format(datetime.now(), time() - start_time))
+
+		else:
+			print("\nGreedy index building at {}".format(datetime.now()))
+    		start_time = time()
+    		proxy.greedy_index_build()
+    		print("Built at {} (took {} seconds)".format(datetime.now(), time() - start_time))
+    	#REMOVE THIS PART
+    	#printing the first n elements of the index
+    	api_proxy.index_head(n=2)
 
 
 	def transport_request(self, identifier):
@@ -26,7 +50,8 @@ class LaunchpadGitLister(SWHListerBase):
 	def transport_response_to_string(self, response):	
 		"""
 		returns a string version of the response of transport_request
-		for logging purposes
+		for logging purposes.
+		We don't need this function as we aren't logging.
 		"""
 		pass
 
@@ -35,10 +60,12 @@ class LaunchpadGitLister(SWHListerBase):
 		Convert the server response into list of a dict for each repo in the
             response, mapping columns in the lister's MODEL class to repo data.
 		"""
-		repos = response.json()
-		return [self.get_model_from_repo(repo) for repo in repos]
+		return [self.get_model_from_repo(response)]
 
 	def get_model_from_repo(self, repo):
+		'''
+		gets the needed fields from the repo object
+		'''
 		return{
 			'uid': repo['unique_name'], 
             'name': repo['name'],		
@@ -50,4 +77,7 @@ class LaunchpadGitLister(SWHListerBase):
 		}
 
 	def transport_quota_check(self, response):
+		'''
+		Handles the server quota. We don't need this as we don't have a server.
+		'''
 		pass
