9c926eb32c5e0830b8af35222f2a4226ba6917ff
joe11093 <joe11093@users.noreply.github.com>
2019-05-21 20:47:47 +0200

    renamed from launchpad_git to launchpad

--- src/launchpad/launchpad_git/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Boolean, Integer
-
-from swh.lister.core.models import ModelBase
-
-class LaunchpadGitModel(ModelBase):
-  """a Launchpad git repository"""
-  __tablename__ = "launchpad_git_repos"
-
-  uid = Column(String, primary_key=True)
